3ea4d4ed32bd3c3512b59d9ea65b1db2aa0c8d40
MalgorzataMarczykzyk <malgorzata_marczyk@wp.pl>
2019-07-14 22:56:54 +0200

    Cards Scripts
    Add two Classes:
    Card (only object card)
    CardVizc (load data from JSON file and show it in game)

---
+++ WarStone/Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public Sprite image;
+        public int hp;
+        public int weaponSkill;
+        public int strength;
+    }
+
+}
---
+++ WarStone/Assets/Scripts/Cards/CardViz.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SA
+{
+    public class CardViz : MonoBehaviour
+    {
+        public TextMeshProUGUI cardName;
+        public TextMeshProUGUI HP;
+        public TextMeshProUGUI strength;
+        public TextMeshProUGUI weaponSkill;
+        public Image image;
+
+        public Card card;
+
+        private void Start()
+        {
+            LoadCard(card);
+        }
+
+        //TODO: Implementation function that retrieves data from JSON file//
+        //Add JSON files in folder \WarStone\Assets\Data//
+        public void LoadCard(Card c)
+        {
+
+        }
+
+
+    }
+}
+
