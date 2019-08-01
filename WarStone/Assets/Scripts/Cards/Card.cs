091f2e4cceca0a380c447171cf1be3d6696680a3
TJoskowiak <tomaszjoskowiak@gmail.com>
2019-08-01 09:27:59 +0200

    Scriptable cards from JSON

--- WarStone/Assets/Scripts/Cards/Card.cs
+++ WarStone/Assets/Scripts/Cards/Card.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//namespace SA
+//{
+//    [CreateAssetMenu(menuName = "Card")]
+//    public class Card : ScriptableObject
+//    {
+//        public string cardName;
+//        public Sprite image;
+//        public int hp;
+//        public int weaponSkill;
+//        public int strength;
+//    }
+
+//}
+
+using System.Collections;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 namespace SA
 {
-    [CreateAssetMenu(menuName = "Card")]
-    public class Card : ScriptableObject
+    [DataContract]
+    public class Card
     {
+        [DataMember]
+        public int id;
+        [DataMember]
         public string cardName;
-        public Sprite image;
+        [DataMember]
+        public string imagePath;
+        [DataMember]
         public int hp;
+        [DataMember]
         public int weaponSkill;
+        [DataMember]
         public int strength;
     }
 
