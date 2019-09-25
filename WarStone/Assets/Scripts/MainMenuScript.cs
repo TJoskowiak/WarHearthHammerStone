4d2c157d147af6b61960a3e288c4a5e17109ec7e
Filip Pietraszak <filippiet@gmail.com>
2019-09-25 17:16:35 +0200

    New card sprites

--- WarStone/Assets/Scripts/MainMenuScript.cs
+++ WarStone/Assets/Scripts/MainMenuScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : NetworkBehaviour
 {
@@ -15,37 +16,52 @@ public class MainMenuScript : NetworkBehaviour
             Destroy(netHandler);
         }
     }
-    public void startGame()
-    {
+    public void startGame() {
 
     }
 
+    public void changeResolution(Dropdown change) {
+        switch (change.value) {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+        }
+    }
+
+    public void toggleFullscreen(Toggle change) {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, change.isOn);
+    }
+
+    public void changeGraphicsSettings(Dropdown change) {
+        QualitySettings.SetQualityLevel(change.value);
+    }
     public void newGamePressed() {
         SceneManager.LoadScene("NetworkManagerScene");
     }
-    public void exitDialogBoxSetActive()
-    {
+    public void exitDialogBoxSetActive() {
         ExitDialogBoxWindow.SetActive(true);
 
     }
-    public void exitDialogBoxPressYes()
-    {
+    public void exitDialogBoxPressYes() {
         Debug.Log("Quit Game by user");
         Application.Quit();
     }
 
-    public void exitDialogBoxPressNo()
-    {
+    public void exitDialogBoxPressNo() {
         ExitDialogBoxWindow.SetActive(false);
     }
 
-    public void settingsDialogBoxSetActive()
-    {
+    public void settingsDialogBoxSetActive() {
         SettingsDialogBoxWindow.SetActive(true);
     }
 
-    public void colseSettingDialogBox()
-    {
+    public void colseSettingDialogBox() {
         SettingsDialogBoxWindow.SetActive(false);
     }
 }
