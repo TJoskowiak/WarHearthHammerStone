9049372b7223b07708b31db5e6ca15b5d0dac533
Filip Pietraszak <filippiet@gmail.com>
2019-09-25 20:19:47 +0200

    New background and hero icon

---
+++ WarStone/Assets/Scripts/Elements/TableElements/HeroIconScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HeroIconScript : MonoBehaviour
+{
+    public GameObject hitpointsText;
+    public GameObject healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePercentage(int value) {
+        hitpointsText.GetComponent<TextMeshProUGUI>().text = value.ToString();
+        healthBar.transform.localScale = new Vector3(value, healthBar.transform.localScale.y);
+
+    }
+}
