5c76986428769468aed39e93e86d40930791cdba
TJoskowiak <tomaszjoskowiak@gmail.com>
2019-09-25 18:30:02 +0200

    Added races

---
+++ WarStone/Assets/Scripts/Races/Dwarves.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA.Races
+{
+    [CreateAssetMenu(menuName = "Race/Dwarves")]
+    public class Dwarves : Race
+    {
+        private readonly List<int> _AvailableCards = new List<int>(15) { 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15 };
+        public override List<int> AvailableCards { get => _AvailableCards; }
+    }
+}
---
+++ WarStone/Assets/Scripts/Races/Race.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA.Races
+{
+    public abstract class Race : ScriptableObject
+    {
+        public abstract List<int> AvailableCards { get; }
+
+        public List<int> ShuffleCards()
+        {
+            List<int> source = new List<int>(AvailableCards);
+            List<int> output = new List<int>();
+            System.Random generator = new System.Random();
+
+            while (source.Count > 0)
+            {
+                int position = generator.Next(source.Count);
+                output.Add(source[position]);
+                source.RemoveAt(position);
+            }
+            return output;
+        }
+    }
+}
