c0c49138a8c46f9635ea59df1ca966a7fee5a098
Filip Pietraszak <filippiet@gmail.com>
2019-09-26 11:16:12 +0200

    Youdead

--- WarStone/Assets/BattleField/MainCameraScript.cs
+++ WarStone/Assets/BattleField/MainCameraScript.cs
@@ -7,6 +7,7 @@ public class MainCameraScript : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start() {
+        this.GetComponent<Animator>().Play("test");
         if (!isServer) {
             transform.Rotate(new Vector3(0, 0, 180));
         }
