35499c4a92be154e6c4babdddaa511cf1f4a448e
Filip Pietraszak <filippiet@gmail.com>
2019-09-26 15:27:50 +0200

    Log window

---
+++ WarStone/Assets/BattleField/LogWindowScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogWindowScript : MonoBehaviour
+{
+    public GameObject Content;
+    public GameObject TextPrefab;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Log(string text) {
+        GameObject newObject = GameObject.Instantiate(TextPrefab);
+        newObject.GetComponent<Text>().text = text;
+
+        newObject.transform.SetParent(Content.transform);
+
+    }
+}
