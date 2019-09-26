7f1b6c1d69eda23927e93f6534fbd18e3751b283
Filip Pietraszak <filippiet@gmail.com>
2019-09-26 15:57:36 +0200

    Log textbox

--- WarStone/Assets/Networking/PlayerConnectionScript.cs
+++ WarStone/Assets/Networking/PlayerConnectionScript.cs
@@ -12,13 +12,10 @@ public class PlayerConnectionScript : NetworkBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        if (isLocalPlayer)
-        {
+        if (isLocalPlayer) {
             this.name = "LocalPlayer";
             SA.Settings.gameManager.SetPlayerConnectionScript(this);
-        }
-        else
-        {
+        } else {
             this.name = "RemotePlayer";
             SA.Settings.gameManager.StartGame();
         }
@@ -28,13 +25,11 @@ public class PlayerConnectionScript : NetworkBehaviour
 
 
     [Command]
-    public void CmdSendMovement(int FirstCard, int SecondCard)
-    {
+    public void CmdSendMovement(int FirstCard, int SecondCard) {
         var Server = GameObject.Find("ServerObject");
         var ServerComp = Server.GetComponent<ServerScript>();
 
-        if (FirstCard != 0 && SecondCard != 0)
-        {
+        if (FirstCard != 0 && SecondCard != 0) {
 
 
             var firstCardObj = GameObject.Find(FirstCard.ToString()) as GameObject;
@@ -43,23 +38,23 @@ public class PlayerConnectionScript : NetworkBehaviour
             RpcSetDMG(secondCardObj, firstCardObj);
 
             ServerComp.RpcMoveCountIncrement();
-            RpcMoveInfo(firstCard, secondCard);
+            var FirstName = firstCardObj.GetComponent<SA.CardViz>().cardName.text;
+            var SecondName = secondCardObj.GetComponent<SA.CardViz>().cardName.text;
+            RpcMoveInfo(FirstName, SecondName);
         }
 
-        
+
 
     }
     [Command]
-    public void CmdRoundOver()
-    {
+    public void CmdRoundOver() {
         var Server = GameObject.Find("ServerObject");
         var ServerComp = Server.GetComponent<ServerScript>();
         ServerComp.RpcSwitchRounds();
     }
 
     [ClientRpc]
-    public void RpcSetDMG(GameObject attacker, GameObject victim)
-    {
+    public void RpcSetDMG(GameObject attacker, GameObject victim) {
         var attackerCard = attacker.GetComponent<SA.CardInstance>();
         var victimCard = victim.GetComponent<SA.CardInstance>();
 
@@ -68,32 +63,20 @@ public class PlayerConnectionScript : NetworkBehaviour
     }
 
     [ClientRpc]
-    public void RpcMoveInfo(int FirstCard, int SecondCard)
-    {
-        var Player1CardComp = GameObject.Find(FirstCard.ToString()).GetComponent<SA.CardViz>();
-        var Player2CardComp = GameObject.Find(SecondCard.ToString()).GetComponent<SA.CardViz>();
+    public void RpcMoveInfo(string FirstCard, string SecondCard) {
 
         string LogMesagge = "";
-        if (SA.Settings.gameManager.currentState == SA.Settings.stateManager.PlayerControlState)
-        {
-            if(SA.Settings.gameManager.currentPlayer.PlayerID == 1)
-            {
-                LogMesagge = Player1CardComp.cardName.text + "has attacked " + Player2CardComp.cardName.text + "./n";
+        if (SA.Settings.gameManager.currentState == SA.Settings.stateManager.PlayerControlState) {
+            if (SA.Settings.gameManager.currentPlayer.PlayerID == 1) {
+                LogMesagge = FirstCard + " attacks " + SecondCard;
+            } else {
+                LogMesagge = SecondCard + " attacks " + FirstCard;
             }
-            else
-            {
-                LogMesagge = Player2CardComp.cardName.text + "has attacked " + Player1CardComp.cardName.text + "./n";
-            }
-        }
-        else
-        {
-            if (SA.Settings.gameManager.currentPlayer.PlayerID == 1)
-            {
-                LogMesagge = Player2CardComp.cardName.text + "has attacked " + Player1CardComp.cardName.text + "./n";
-            }
-            else
-            {
-                LogMesagge = Player1CardComp.cardName.text + "has attacked " + Player2CardComp.cardName.text + "./n";
+        } else {
+            if (SA.Settings.gameManager.currentPlayer.PlayerID == 1) {
+                LogMesagge = SecondCard + " attacks " + FirstCard;
+            } else {
+                LogMesagge = FirstCard + " attacks " + SecondCard;
             }
         }
         GameObject.Find("LogWindow").GetComponent<LogWindowScript>().Log(LogMesagge);
@@ -102,20 +85,17 @@ public class PlayerConnectionScript : NetworkBehaviour
     }
 
     [Command]
-    public void CmdEndGame(int LoserPlayerID)
-    {
+    public void CmdEndGame(int LoserPlayerID) {
         RpcEndGame(LoserPlayerID);
     }
 
     [ClientRpc]
-    public void RpcEndGame(int LoserPlayerID)
-    {
+    public void RpcEndGame(int LoserPlayerID) {
         SA.Settings.gameManager.setEndScreen(LoserPlayerID);
     }
 
     [Command]
-    public void CmdCardDeployed(GameObject card, int PlayerID)
-    {
+    public void CmdCardDeployed(GameObject card, int PlayerID) {
         var Server = GameObject.Find("ServerObject");
         var ServerComp = Server.GetComponent<ServerScript>();
         ServerComp.RpcCardDeployed(card, PlayerID);
@@ -123,34 +103,30 @@ public class PlayerConnectionScript : NetworkBehaviour
     }
 
     [Command]
-    public void CmdSpawnCard(int PlayerID)
-    {
+    public void CmdSpawnCard(int PlayerID) {
         GameObject cardObj = Instantiate(SA.Settings.gameManager.CardPrefab) as GameObject;
         NetworkServer.Spawn(cardObj);
         SA.Settings.gameManager.GetPlayer(PlayerID).AssignParametersToCard(cardObj);
         SA.CardViz viz = cardObj.gameObject.GetComponentInParent<SA.CardViz>();
-        RpcSetToHand(cardObj, viz.card_object_id , viz.card_json_id ,PlayerID);
+        RpcSetToHand(cardObj, viz.card_object_id, viz.card_json_id, PlayerID);
         RpcPickCard(PlayerID);
         RpcCheckCardLeft(PlayerID);
     }
 
     [ClientRpc]
-    public void RpcPickCard(int PlayerID)
-    {
+    public void RpcPickCard(int PlayerID) {
         SA.Settings.gameManager.GetPlayer(PlayerID).removeCardFromDeckCounter();
     }
 
 
     [ClientRpc]
-    public void RpcCheckCardLeft(int PlayerID)
-    {
+    public void RpcCheckCardLeft(int PlayerID) {
         if (!SA.Settings.gameManager.GetPlayer(PlayerID).CheckIfAnyCardLeft())
             SA.Settings.gameManager.GetPlayer(PlayerID).hideDeck();
     }
 
     [ClientRpc]
-    public void RpcSetToHand(GameObject card, int CardID, int CardJsonID, int PlayerID)
-    {
+    public void RpcSetToHand(GameObject card, int CardID, int CardJsonID, int PlayerID) {
         if (card == null) return;
         SA.CardViz viz = card.gameObject.GetComponentInParent<SA.CardViz>();
         viz.card_json_id = CardJsonID;
@@ -163,31 +139,23 @@ public class PlayerConnectionScript : NetworkBehaviour
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if(firstCard != 0 && secondCard != 0)
-        {
+    void Update() {
+        if (firstCard != 0 && secondCard != 0) {
             CmdSendMovement(firstCard, secondCard);
 
-            try
-            {
+            try {
                 var firstViz = GameObject.Find(firstCard.ToString()).GetComponent<SA.CardViz>();
                 var secondViz = GameObject.Find(secondCard.ToString()).GetComponent<SA.CardViz>();
 
                 firstViz._highlight = false;
                 secondViz._highlight = false;
 
-                if(isServer)
-                {
+                if (isServer) {
                     firstViz.cardMoved = true;
-                }
-                else
-                {
+                } else {
                     secondViz.cardMoved = true;
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Debug.Log(e);
             }
             firstCard = 0;
